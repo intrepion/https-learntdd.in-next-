c2cbf335fdd2b3c0adb9105c0e710951cb6290c7
Oliver Forral <intrepion@gmail.com>
2024-04-06 14:32:18 -0600

    A List

--- learn-tdd-in-next/src/app/page.tsx
+++ learn-tdd-in-next/src/app/page.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import { useState } from "react";
 import NewMessageForm from "../components/NewMessageForm";
+import MessageList from "../components/MessageList";
 
 export default function Home() {
-  function handleSend() {}
+  const [messages, setMessages] = useState([]);
+  function handleSend(newMessage) {
+    setMessages([newMessage, ...messages]);
+  }
 
-  return <NewMessageForm onSend={handleSend} />;
+  return (
+    <>
+      <NewMessageForm onSend={handleSend} />
+      <MessageList data={messages} />
+    </>
+  );
 }
